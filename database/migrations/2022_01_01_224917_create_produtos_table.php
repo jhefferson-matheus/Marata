01f6b7b0a397e693261f26e19b062ace2bd8b8f4
Jhefferson <30627714+jhefferson-matheus@users.noreply.github.com>
2022-01-01 20:04:46 -0300

    Update 2022_01_01_224917_create_produtos_table.php
    Modificação dos dados da função up() campos de produtos

--- database/migrations/2022_01_01_224917_create_produtos_table.php
+++ database/migrations/2022_01_01_224917_create_produtos_table.php
@@ -13,12 +13,15 @@ class CreateProdutosTable extends Migration
      */
     public function up()
     {
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('produtos', function (Blueprint $table){
+            $table->id(); //ID dos produtos
+            $table->string('nome'); //Campo o qual espera uma entrada de texto com o nome do produto
+            $table->decimal('custo', 19, 2); //Campo o qual esperar uma entrada decimal detinada ao custo do produto
+            $table->decimal('preco', 19, 2); //Campo reservado para entrada decimal dos preços dos produtos
+            $table->integer('quantidade'); //Entrada de texto reservada para números inteiros sobre a quantidade de produtos
+            $table->timestamps(); //Carimbo de data e hora do laravel
         });
     }
-
     /**
      * Reverse the migrations.
      *
