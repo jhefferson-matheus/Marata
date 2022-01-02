60e24307b4721faa746f28b1576d041f69846937
Jhefferson <30627714+jhefferson-matheus@users.noreply.github.com>
2022-01-02 14:37:48 -0300

    Tela inicial
    Modifiquei  a tela index de boas vindas para acessar as rotas mais facilmente e testar o sistema, tela index só será feita nesse projeto de forma a ter um bom front-end caso sobre tempo

--- resources/views/createProdutos.blade.php
+++ resources/views/createProdutos.blade.php
@@ -17,7 +17,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">GitHub</a>
+        <a class="nav-item nav-link active" href="#">Repositório</a>
         </div>
     </div>
     </nav>
