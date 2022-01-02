e683b36c007f87817648062eccf285f3bc1e19e5
Jhefferson <30627714+jhefferson-matheus@users.noreply.github.com>
2022-01-02 14:11:19 -0300

    Bootstrap devidiamente inserido
    ficou muito evidente como modificar e chamara qualquer elemento usando o laravel, isso estava me dando uma certa dúvida, mas peguei uma referência ótima.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+.sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/web/bootstrap.css')
+
+.scripts('node_modules/jquery/dist/jquery.js', 'public/web/jquery.js')
+.scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/web/bootstrap.js');
