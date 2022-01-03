97003b298de153837b2bfae473ef2a5614f1b049
Jhefferson <30627714+jhefferson-matheus@users.noreply.github.com>
2022-01-03 13:45:40 -0300

    Update web.php
    Criando todas as rotas de pedido - é claro que com uma parte do crud já completa o crud de pedidos será mais rápido porque será muito reaproveitamento de código

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutosController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/*                               ROTAS WEB
+|--------------------------------------------------------------------------|
+|                               TELA INICIAL                               |
+|--------------------------------------------------------------------------|
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,7 +15,11 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-
+/*
+|--------------------------------------------------------------------------|
+|                                 PRODUTOS                                 |
+|--------------------------------------------------------------------------|
+*/
 Route::get('/produtos/novo', [ProdutosController::class, 'create']);
 Route::post('/produtos/novo', [ProdutosController::class, 'store'])->name('registrar_produto');
 Route::get('/produtos/ver/{produto}', [ProdutosController::class, 'show']);
@@ -29,8 +27,18 @@ Route::get('/produtos/edit/{produto}', [ProdutosController::class, 'edit']);
 Route::post('/produtos/edit/{produto}', [ProdutosController::class, 'update'])->name('atualizar');
 Route::get('/produtos/drop/{produto}', [ProdutosController::class, 'delete']);
 Route::post('/produtos/drop/{produto}', [ProdutosController::class, 'destroy'])->name('deletar');
-
-
+/*
+|--------------------------------------------------------------------------|
+|                                  PEDIDOS                                 |
+|--------------------------------------------------------------------------|
+*/
+Route::get('/pedidos/novo', [PedidosController::class, 'create']);
+Route::post('/pedidos/novo', [PedidosController::class, 'store'])->name('registrar_pedidos');
+Route::get('/pedidos/ver/{pedidos}', [PedidosController::class, 'show']);
+Route::get('/pedidos/edit/{pedidos}', [PedidosController::class, 'edit']);
+Route::post('/pedidos/edit/{pedidos}', [PedidosController::class, 'update'])->name('atualizar');
+Route::get('/pedidos/drop/{pedidos}', [PedidosController::class, 'delete']);
+Route::post('/pedidos/drop/{pedidos}', [PedidosController::class, 'destroy'])->name('deletar');
 
 
 
